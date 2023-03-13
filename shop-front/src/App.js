f7eb75f3b304756ed1c2eaf1d4d5096df1843749
Seif2001 <seifelshabshiri@aucegypt.edu>
2023-03-13 11:51:17 +0200

    created react app

---
+++ shop-front/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/"> My shop</a>
+      </header>
+      <main>list prtoducts</main>
+    </div>
+  );
+}
+
+export default App;
